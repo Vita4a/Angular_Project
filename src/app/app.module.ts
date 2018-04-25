45d16d039a7d094fd188d8e91ded69ef32413c73
Vita4a <vera.dyafu@gmail.com>
2018-04-25 18:04:29 +0200

    SPA Part 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
 
 
 import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
 
 import 'hammerjs';
 
@@ -43,7 +44,7 @@ import { ContactComponent } from './contact/contact.component';
     BrowserAnimationsModule,
     AppRoutingModule
   ],
-  providers: [DishService],
+  providers: [DishService, PromotionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
