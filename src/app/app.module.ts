f197209697122919c7bef7de9df1906c044fa26b
Vita4a <vera.dyafu@gmail.com>
2018-04-28 19:22:29 +0200

    Assingment 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,7 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
 
 import { DishService } from './services/dish.service';
 import { PromotionService } from './services/promotion.service';
+import { LeaderService } from './services/leader.service';
 
 import 'hammerjs';
 
@@ -44,7 +45,7 @@ import { ContactComponent } from './contact/contact.component';
     BrowserAnimationsModule,
     AppRoutingModule
   ],
-  providers: [DishService, PromotionService],
+  providers: [DishService, PromotionService, LeaderService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
